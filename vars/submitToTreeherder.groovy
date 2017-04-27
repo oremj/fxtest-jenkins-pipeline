232edad69e2f7cfd3d87ee761ca2fbee8c4a1d77
Jeremiah Orem <oremj@oremj.com>
2017-04-27 16:42:07 -0700

    no publish

--- vars/submitToTreeherder.groovy
+++ vars/submitToTreeherder.groovy
@@ -43,7 +43,7 @@ def call(String project,
   exchange = "exchange/${PULSE_USR}/jobs"
   routingKey = "${PULSE_USR}.${payload.productName}"
   schema = libraryResource 'org/mozilla/fxtest/pulse/schemas/treeherder.yml'
-  publishToPulse(exchange, routingKey, JsonOutput.toJson(payload), schema)
+ // publishToPulse(exchange, routingKey, JsonOutput.toJson(payload), schema)
   treeherderURL = "https://treeherder.mozilla.org/#/jobs?repo=${payload.productName}&revision=${payload.origin.revision}"
   echo "Results will be available to view at $treeherderURL"
 }

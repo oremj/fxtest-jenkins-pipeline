7b4f8986700af4219aa3c939652bfc17567b4b56
Jeremiah Orem <oremj@oremj.com>
2017-04-27 17:51:11 -0700

    loop

--- vars/threadLeak.groovy
+++ vars/threadLeak.groovy
@@ -1,4 +1,6 @@
 def call() {
-    def jsonSchemaValidator = new org.mozilla.fxtest.JsonSchemaValidator()
-    jsonSchemaValidator.validate("", "")
+    for(i in 1..1000) {
+        def jsonSchemaValidator = new org.mozilla.fxtest.JsonSchemaValidator()
+        jsonSchemaValidator.validate("", "")
+    }
 }
